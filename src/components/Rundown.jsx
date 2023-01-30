d28f932906f27799263d3f58ccc2480d88237b07
dzaky1201 <dzakyhdr@gmail.com>
2023-01-30 08:53:37 +0700

    show jadwal kajian

--- src/components/Rundown.jsx
+++ src/components/Rundown.jsx
@@ -1,3 +1,110 @@
-export default function RundownPage() {
-  return <h1 style={{textAlign:"center"}}>this is rundown page</h1>;
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import statusOn from ".././img/green-status.png";
+import statusOff from ".././img/red-status.png";
+import {
+  Box,
+  Card,
+  CardContent,
+  CircularProgress,
+  CssBaseline,
+  Divider,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  Typography,
+} from "@mui/material";
+import axios from "axios";
+import { Circle } from "@mui/icons-material";
+import { green } from "@mui/material/colors";
+
+const ResponsiveBox = styled(Box)(({ theme }) => ({
+  [theme.breakpoints.up("md")]: {
+    width: 488,
+    marginLeft: "auto",
+    marginRight: "auto",
+  },
+}));
+
+const baseURL = "https://nurul-iman-blok-m.herokuapp.com/api/v1/rundown";
+
+export default function HomePage() {
+  const [rundown, setRundown] = React.useState("");
+
+  React.useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      setRundown(response.data);
+    });
+  }, []);
+
+  if (!rundown) {
+    return (
+      <Box sx={{ display: "flex" }}>
+        <CircularProgress sx={{ margin: "auto", color: "green" }} />
+      </Box>
+    );
+  } else {
+    return (
+      <ResponsiveBox>
+        <CssBaseline />
+
+        {rundown.data.map(({ title, ustadz_name, date, time, on_scheduled }, index) => (
+          <List>
+            <ListItem button key={index + title}>
+              <Box sx={{ width: "100%" }}>
+                <Card variant="outlined" sx={{borderRadius:"14px"}}>
+                  <CardContent>
+                    <Typography sx={{ fontSize: "16px", fontWeight: "500" }}>
+                      {title}
+                    </Typography>
+                    <Typography
+                      sx={{
+                        fontSize: "12px",
+                        fontWeight: "400",
+                        marginTop: "13px",
+                      }}>
+                      {ustadz_name}
+                    </Typography>
+                    <Box
+                      sx={{ display: "flex", justifyContent: "space-between" }}>
+                      <Typography
+                        sx={{
+                          fontSize: "11px",
+                          fontWeight: "400",
+                          marginTop: "9px",
+                          color: "#818181",
+                        }}>
+                        {date}
+                      </Typography>
+                      <Typography
+                        sx={{
+                          fontSize: "11px",
+                          fontWeight: "400",
+                          marginTop: "9px",
+                        }}>
+                        {time}
+                      </Typography>
+                    </Box>
+                    <Box sx={{ display: "flex", justifyContent:"end" }}>
+                    { on_scheduled ? <Circle sx={{width:'9px', height:"9px", color:"green", marginTop:"13px"}}/> : <Circle sx={{width:'9px', height:"9px", color:"red", marginTop:"13px"}}/>}
+                      <Typography
+                        sx={{
+                          fontSize: "11px",
+                          fontWeight: "400",
+                          marginTop: "9px",
+                          marginLeft:"4px"
+                        }}>
+                        {on_scheduled ? "Terjadwal" : "dibatalkan"}
+                      </Typography>
+                    </Box>
+                  </CardContent>
+                </Card>
+              </Box>
+            </ListItem>
+          </List>
+        ))}
+      </ResponsiveBox>
+    );
+  }
 }
